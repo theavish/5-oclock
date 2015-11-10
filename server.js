88f03277f1fe39201e37ee5c7c5da52068b34a74
Avi Samloff <avi.samloff@gmail.com>
2015-11-09 17:12:52 -0800

    refactors server code

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 var express = require('express');
 var app = express();
 var server = app.listen(3000, function () {
-  var host = server.address().address;
   var port = server.address().port;
-
   console.log('http://localhost:' + port);
 });
 
 app.use(express.static(__dirname + '/client/'));
-
-
-// app.get('/', function(req,res) {
-//   res.send('richhomie')
-// });
